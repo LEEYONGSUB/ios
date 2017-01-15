3e319c10261fa7645db9539634c7834838e2b74f
leonardo204 <zerolive7@gmail.com>
2017-01-15 14:53:17 +0900

    Initial Commit

---
+++ TrustPay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TrustPay
+//
+//  Created by zerolive on 2017. 1. 15..
+//  Copyright © 2017년 Leonardo204. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
