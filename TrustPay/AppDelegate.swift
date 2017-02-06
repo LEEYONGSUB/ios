8baccd2f71c2f9c65a6e10f51f0233b23271da83
leonardo204 <zerolive7@gmail.com>
2017-02-06 21:19:02 +0900

    - Firebase 추가

--- TrustPay/AppDelegate.swift
+++ TrustPay/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        let ref = FIRDatabase.database().reference()
+        let itemRef = ref.child("TEST")
+        itemRef.setValue(["Name":"Tilltue"])
         return true
     }
 
